2dabab752e8ccaa02789a1d19478cce2c5cc48b1
Irvan Junaidi <49930719+irvanswan@users.noreply.github.com>
2021-06-18 10:36:20 +0000

    Change fizzbuzz.js with the function of fizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,23 @@
 const fizzBuzz = function(data) {
-    console.log('Silahkan ubah file fizzbuzz.js');
+    if(typeof(data) !== 'object'){
+      console.log("data bukan array")
+    }else{
+      if(data.length > 0){
+        for(let i=0; i<data.length;i++){
+          if(data[i] % 5 ==0 && data[i]%3 == 0){
+            console.log(data[i]," FizzBuzz")
+          }else if(data[i] % 5 == 0){
+            console.log(data[i]," Buzz")
+          }else if(data[i] % 3 == 0){
+            console.log(data[i]," Fizz")
+          }else{
+            console.log(data[i])
+          }
+        }
+      }else{
+        console.log('tidak ada datanya')
+      }
+    }
 }
 
 module.exports = fizzBuzz;
